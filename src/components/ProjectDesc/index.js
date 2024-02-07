5ff6379e2eb747aa9ef467b5a57127f9f26042b3
RinaHan <rinahan4146@gmail.com>
2024-02-06 16:06:29 -0800

    create ProjectDesc component

---
+++ src/components/ProjectDesc/index.js
@@ -0,0 +1,74 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import "./ProjectDesc.css";
+import { Button } from "../Button";
+
+function ProjectDesc(props) {
+  // const test = props.exp4.length;
+  // console.log('test: ', test);
+
+  return (
+    <div className='p-cont'>
+      {/* -----description section */}
+      <section className='p-desc-cont'>
+        {/* leftside image */}
+        <div className='p-img-wrap'>
+          <img className='p-img' src={props.src} alt='project' />
+        </div>
+        {/* rightside details */}
+        <div className='p-details-wrap'>
+          <p className='p-title'>{props.title}</p>
+          <ul className='p-details'>
+            <li id='p-bold-text'>
+              {props.sort} {props.sortIcon}
+            </li>
+            <li id='p-bold-text'>
+              {props.tool1} {props.toolIcon1}
+            </li>
+            <li id='p-bold-text'>
+              {props.tool2} {props.toolIcon2}
+            </li>
+            <li>{props.exp1}</li>
+            <li>{props.exp2}</li>
+            <li>{props.exp3}</li>
+            {/* <li>{props.exp4}</li> */}
+            {/* {props.exp4.length >= 0 && <li>test</li>} */}
+          </ul>
+
+          {/* buttons */}
+          <div className='p-btn-wrap'>
+            <a href={props.githubUrl}>
+              <Button className='btn' buttonStyle='btn--outline' buttonSize='btn--medium'>
+                GitHub <i className='fab fa-github' />
+              </Button>
+            </a>
+            {/* maybe later */}
+            {/* <a>
+              <Button className='btn' buttonStyle='btn--outline' buttonSize='btn--medium'>
+                Test <i className='fas fa-play-circle' />
+              </Button>
+            </a> */}
+          </div>
+        </div>
+      </section>
+
+      {/* -----demo section */}
+      <section className='p-demo-cont'>
+        <div className='p-section-title'>Demo</div>
+        <div className='p-demo-video'>{props.demo}</div>
+      </section>
+
+      {/* -----code section */}
+      <section className='p-code-cont'>
+        <div className='p-section-title'>Code</div>
+        <div className='p-code-img-wrap'>
+          <img src={props.projectSrc1} alt='' />
+          <img src={props.projectSrc2} alt='' />
+          <img src={props.projectSrc3} alt='' />
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default ProjectDesc;

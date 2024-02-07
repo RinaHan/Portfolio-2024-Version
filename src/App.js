4aa66f2df6cd12cd9212296474da12015318733c
RinaHan <rinahan4146@gmail.com>
2024-02-07 06:39:38 -0800

    create Disney and TicTacToe pages

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import GreenLife from "./pages/GreenLife";
 import Linkt from "./pages/Linkt";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import TicTacToe from "./pages/TicTacToe";
+import DisneyPlus from "./pages/DisneyPlus";
 
 const Layout = () => {
   return (
@@ -23,10 +25,12 @@ function App() {
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path='volunteers' element={<Volunteers />} />
+          <Route path='disneyplus' element={<DisneyPlus />} />
+          <Route path='tictactoe' element={<TicTacToe />} />
           <Route path='walk' element={<Walk />} />
-          <Route path='greenLife' element={<GreenLife />} />
+          <Route path='volunteers' element={<Volunteers />} />
           <Route path='Linkt' element={<Linkt />} />
+          <Route path='greenLife' element={<GreenLife />} />
         </Route>
       </Routes>
     </div>
---
+++ src/pages/TicTacToe.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ProjectDesc from "../components/ProjectDesc";
+
+function TicTacToe() {
+  return (
+    <div>
+      <ProjectDesc
+        src='images/tictactoe.png'
+        title='Tic-Tac-Toe'
+        subtitle='2024'
+        sort='Web App'
+        sortIcon={<i className='fas fa-mobile-alt' />}
+        exp1='Simple tic tac toe service'
+        exp2='Functional component using React Hooks'
+        exp3='Single page application with the basic structure and functions of React'
+        exp4='Deployed using Github'
+        tool1='React'
+        toolIcon1={<i className='fab fa-react'></i>}
+        tool2='Node'
+        toolIcon2={<i className='fab fa-node'></i>}
+        // toolIcon2={<i className='fab fa-npm'></i>}
+        githubUrl='https://github.com/RinaHan/tic-tac-toe'
+        testUrl='https://rinahan.github.io/tic-tac-toe/'
+        demo={<video src='/videos/tictactoe.mp4' autoPlay loop muted />}
+        projectSrc1='images/tictactoe-code.png'
+        // projectSrc2='images/green2.png'
+        // projectSrc3="images/linkt4.png"
+      />
+    </div>
+  );
+}
+
+export default TicTacToe;

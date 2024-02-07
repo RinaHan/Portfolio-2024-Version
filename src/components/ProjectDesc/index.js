250fbf1d6914a01c98a09c41056c7fdc4d9838c6
RinaHan <rinahan4146@gmail.com>
2024-02-07 06:38:39 -0800

    fix ProjectDesc comp style

--- src/components/ProjectDesc/index.js
+++ src/components/ProjectDesc/index.js
@@ -9,45 +9,46 @@ function ProjectDesc(props) {
       {/* -----description section */}
       <section className='p-desc-section'>
         <div className='p-desc-cont'>
-        {/* leftside-image */}
-        <div className='p-img-wrap'>
-          <img src={props.src} alt='project' />
-        </div>
-        {/* rightside-details */}
-        <div className='p-details-wrap'>
-          <p className='p-title'>{props.title}</p>
-          <ul className='p-details'>
-          <li className='p-sub-title'>{props.subtitle}</li>
-            <li id='p-bold-text'>
-              {props.sort} {props.sortIcon}
-            </li>
-            <li id='p-bold-text'>
-              {props.tool1} {props.toolIcon1}
-            </li>
-            <li id='p-bold-text'>
-              {props.tool2} {props.toolIcon2}
-            </li>
-            <li>{props.exp1}</li>
-            <li>{props.exp2}</li>
-            <li>{props.exp3}</li>
-            {/* <li>{props.exp4}</li> */}
-            {/* {props.exp4.length >= 0 && <li>test</li>} */}
-          </ul>
-
-          {/* buttons */}
-          <div className='p-btn-wrap'>
-            <a href={props.githubUrl} target='_blank' rel="noreferrer" >
-              <Button className='btn' buttonStyle='btn--outline' buttonSize='btn--medium'>
-                GitHub <i className='fab fa-github' />
-              </Button>
-            </a>
-            {/* <a>
-              <Button className='btn' buttonStyle='btn--outline' buttonSize='btn--medium'>
-                Test <i className='fas fa-play-circle' />
-              </Button>
-            </a> */}
+          {/* left-side-image */}
+          <div className='p-img-wrap'>
+            <img src={props.src} alt='project' />
           </div>
-        </div>
+          {/* right-side-details */}
+          <div className='p-details-wrap'>
+            <p className='p-title'>{props.title}</p>
+            <ul className='p-details'>
+              <li className='p-sub-title'>{props.subtitle}</li>
+              <li id='p-bold-text'>
+                {props.sort} {props.sortIcon}
+              </li>
+              <li id='p-bold-text'>
+                {props.tool1} {props.toolIcon1}
+              </li>
+              <li id='p-bold-text'>
+                {props.tool2} {props.toolIcon2}
+              </li>
+              <li>{props.exp1}</li>
+              <li>{props.exp2}</li>
+              <li>{props.exp3}</li>
+              {props.exp4 && <li>{props.exp4}</li>}
+            </ul>
+
+            {/* buttons */}
+            <div className='p-btn-wrap'>
+              <a href={props.githubUrl} target='_blank' rel='noreferrer'>
+                <Button buttonStyle='btn--outline' buttonSize='btn--medium'>
+                  GitHub <i className='fab fa-github' />
+                </Button>
+              </a>
+              {props.testUrl && (
+                <a href={props.testUrl} target='_blank' rel='noreferrer'>
+                  <Button buttonStyle='btn--outline' buttonSize='btn--medium'>
+                    Test <i className='fas fa-play-circle' />
+                  </Button>
+                </a>
+              )}
+            </div>
+          </div>
         </div>
       </section>
 
